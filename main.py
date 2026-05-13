d0e7dab4c19b055e6350e5581a37d865a2b31c62
kremalhlal56-ctrl <kremalhlal56@gmail.com>
2026-05-14 00:36:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# هذا السطر هو اللي يشغل الخدعة
+keep_alive()
 import telebot
 import requests
 import yt_dlp
